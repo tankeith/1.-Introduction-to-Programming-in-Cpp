5261684f8b8e8658dfb0ec2b4e3e3d9d691572aa
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-04-08 12:56:37 +0800

    updated fibonacci sequence to start with 1

--- 7. Labs/2. fibonacci/fibonacci.cpp
+++ 7. Labs/2. fibonacci/fibonacci.cpp
@@ -1,5 +1,5 @@
 // The Fibonacci sequence is a series of numbers in which each number is the sum of the preceding two numbers. 
-// The first two numbers in the series are the number 1. 
+// The first two numbers in the series are the number 1. // <-- specific to assignment. Fibs start with 0
 // Write a program to print the first n Fibonacci numbers.
 // Please enter a positive integer greater than 1: 4
 // 1
@@ -18,7 +18,7 @@ int main() {
     cout << "Please enter a positive integer greater than 1: ";
     cin >> userInput;
 
-    n1 = 0;
+    n1 = 1;
     n2 = 1;
     cout << n1 << endl;
     cout << n2 << endl;
